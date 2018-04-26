5a39007ce439144aee29b6a0701af3df6674f229
patchkez@gmail.com <patchkez@gmail.com>
2018-04-26 16:25:06 +0200

    madness

--- iguana/coins/tmp_cleanup/separate_json_data-yml.sh
+++ iguana/coins/tmp_cleanup/separate_json_data-yml.sh
@@ -24,9 +24,9 @@ SUPPLIES=(`curl $URL -s | grep "^komodo_asset" | awk '{print $2 " " $3 }'`)
 echo "assetchains:"
 for coin in `ls -1 *_7776`; do
     # echo  "Processing ==== $coin ===="
-    coin_name=`echo ${coin} | sed 's/_7776//g' | tr '[:lower:]' '[:upper:]'`   
+    coin_name=`echo ${coin} | sed 's/_7776//g' | tr '[:lower:]' '[:upper:]'`
     echo "  ${coin_name}:"
-    echo "    iguana_payload:" 
+    echo "    iguana_payload:"
     cat $coin | \
 	sed -n 's/^.*data \(.*\)$/\1/gp' | \
 	sed -e 's/,/\n/g' | \
@@ -37,6 +37,18 @@ for coin in `ls -1 *_7776`; do
 	    -e 's/}"//g' \
 	    -e 's/"\/"/\//g' \
 	    -e 's/^/      /g' | \
+  sed -e '/genesis: /{
+      $!{N;N;N;N;
+      s/\n/\n  /g
+      s/ {version: \(.*\)/\n        version: \1/
+      s/}//g
+      t label-yes
+      :label-no
+      P
+      D
+      :label-yes
+      }
+    }'| \
 	sed '/genesisblock: /{s/genesisblock: \(.[^\n]*\)/genesisblock: >\n        \1/g; s/.\{90\}/&\n        /g}'
 
     LIST="" 
@@ -48,8 +60,20 @@ for coin in `ls -1 *_7776`; do
 	fi
     done
     if [ -n "$LIST" ];then
-	echo "    supply: $LIST" 
+	echo "    supply: $LIST"
     else
 	echo "    supply:"
-    fi 	
+  echo
+fi
 done
+
+      # s/genesis: {\(.*\)\n\(.*\)/genesis:\n        \1\n  \2  /g
+      #s/genesis: \(.*\)$\n.*timestamp: \(.*\)$/genesis:\1\2/g
+# sed -n '/genesis: /{
+#             $!{ N
+#               s/genesis: \(.[^\\}]*\).*/genesis:\n          \1/gp
+#               t label-yes
+#               :label-no
+#               P
+#               D
+#               :label-yes
